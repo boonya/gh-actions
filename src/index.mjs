784e8730662067784bd0d1de7bf11ccf2f731e7c
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2023-03-13 01:14:10 +0100

    First try

---
+++ src/index.mjs
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import core from '@actions/core';
+import github from '@actions/github';
+
+try {
+  const file = core.getInput('file');
+  const repo = core.getInput('repo');
+  const homepage = core.getInput('homepage');
+  const ref = core.getInput('ref');
+  const label = core.getInput('label');
+  const sha = core.getInput('sha');
+
+  console.log(`Hello my action!`, {
+    file,
+    repo,
+    homepage,
+    ref,
+    label,
+    sha,
+  });
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
+
