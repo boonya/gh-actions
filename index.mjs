c7c7b923b7a221fbc4e1b675b45c52f5a07cd73b
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2023-03-13 12:26:37 +0100

    One more try

--- index.mjs
+++ index.mjs
@@ -4,50 +4,54 @@ import path from 'path';
 
 const log = console;
 
-async function parseFile(file) {
-	try {
-	// eslint-disable-next-line security/detect-non-literal-fs-filename
-		const content = await readFile(new URL(file, import.meta.url));
-		return JSON.parse(content);
-	}
-	catch (err) {
-		log.warn('parseFile failed.', err);
-		return {};
-	}
+async function parseFile(catalog) {
+  try {
+    const filepath = path.resolve(catalog);
+    // eslint-disable-next-line security/detect-non-literal-fs-filename
+    const content = await readFile(new URL(filepath, import.meta.url));
+    return JSON.parse(content);
+  }
+  catch (err) {
+    log.warn('parseFile failed.', err);
+    return {};
+  }
 }
 
 function redoBuilds(props, builds = []) {
-	return [...builds.filter((build) => build.label !== props.label), {
-		ref: props.ref,
-		label: props.label,
-		sha: props.sha,
-		updated: new Date().toISOString(),
-	}];
+  if (removing) {
+    return builds.filter((build) => build.ref !== props.ref);
+  }
+  return [...builds.filter((build) => build.ref !== props.ref), {
+    ref: props.ref,
+    label: props.label,
+    sha: props.sha,
+    updated: new Date().toISOString(),
+  }];
 }
 
 try {
-  const file = core.getInput('file');
+  const catalog = core.getInput('catalog');
+  const removing = core.getInput('remove');
+  const ref = core.getInput('ref');
+  const sha = core.getInput('sha');
+  const label = core.getInput('label');
   const repo = core.getInput('repo');
   const homepage = core.getInput('homepage');
-  const ref = core.getInput('ref');
-  const label = core.getInput('label');
-  const sha = core.getInput('sha');
 
   log.info(`Hello my action!`, {
-    file,
+    catalog,
+    removing,
+    ref,
+    sha,
+    label,
     repo,
     homepage,
-    ref,
-    label,
-    sha,
   });
 
-  const filepath = path.resolve(file);
-
-  const content = await parseFile(filepath);
+  const content = await parseFile(catalog);
   log.info('This is the current content:', content);
 
-  const builds = redoBuilds({ref,label,sha}, content?.builds);
+  const builds = redoBuilds({ref, label, sha, removing}, content?.builds);
 
   const newContent = {
     builds,
@@ -60,7 +64,7 @@ try {
   const string = JSON.stringify(newContent, undefined, 2);
 
   // eslint-disable-next-line security/detect-non-literal-fs-filename
-  await writeFile(file, string);
+  await writeFile(catalog, string);
 
   log.info('Catalog script finished.');
 } catch (error) {
